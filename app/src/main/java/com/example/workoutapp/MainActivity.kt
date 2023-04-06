9d59a5083b847d33c2de28fd999f6fbb8db4eef7
Prasoon <pp209738@gmail.com>
2023-04-07 01:54:47 +0530

    Last

--- app/src/main/java/com/example/workoutapp/MainActivity.kt
+++ app/src/main/java/com/example/workoutapp/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.workoutapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.FrameLayout
-import android.widget.Toast
 import com.example.workoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
